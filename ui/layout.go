7177b3195f9e0f3ea8af84e15403247bb56d25c8
J2mF <mp4-12cs5@hotmail.com>
2022-09-23 14:40:26 +0300

    Add color picker

--- ui/layout.go
+++ ui/layout.go
@@ -1,7 +1,12 @@
 package ui
 
+import "fyne.io/fyne/v2/container"
+
 func Setup(app *AppInit) {
 	swatchesContainer := BuildSwatches(app)
+	colorPicker := SetupColorPicker(app)
 
-	app.PixelWindow.SetContent(swatchesContainer)
+	appLayout := container.NewBorder(nil, swatchesContainer, nil, colorPicker)
+
+	app.PixelWindow.SetContent(appLayout)
 }
